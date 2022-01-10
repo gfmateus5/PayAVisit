cf56df764ebd88c5a354606178e0a0de32ec8f09
ist193700 <ist.user@tecnico.ulisboa.pt>
2022-01-10 14:10:20 +0000

    routes pages modifications

--- lib/pages/most_famous_spots.dart
+++ lib/pages/most_famous_spots.dart
@@ -6,16 +6,19 @@ class MostFamousSpotsPage extends StatefulWidget {
 }
 class MostFamousSpot {
   String text;
-  MostFamousSpot({this.text});
+  String url;
+  Function onClick;
+
+  MostFamousSpot({this.text, this.url, this.onClick});
 }
 class _MostFamousSpotsPage extends State<MostFamousSpotsPage> {
 
   List<MostFamousSpot> spots = [
-    MostFamousSpot(text: 'Must See Spots in Central Lisbon'),
-    MostFamousSpot(text: 'Amadora Scenic View'),
-    MostFamousSpot(text: 'Get To Know Sintra'),
-    MostFamousSpot(text: 'Walk Around Cascais'),
-    MostFamousSpot(text: 'The Southern Margin of the Tagus')
+    MostFamousSpot(text: 'Central Lisbon', url: 'https://www.lisbonguru.com/wp-content/uploads/2017/12/praca-do-comercio.jpg'),
+    MostFamousSpot(text: 'Amadora Scenic View', url: 'https://upload.wikimedia.org/wikipedia/commons/9/9e/Alfragide_norte%2C_outubro_2017_%28cropped%29.jpg'),
+    MostFamousSpot(text: 'Get To Know Sintra', url: 'https://upload.travelawaits.com/ta/uploads/2021/05/sintra-cover.jpg'),
+    MostFamousSpot(text: 'Walk Around Cascais', url: 'https://www.eurodicas.com.br/wp-content/uploads/2019/05/morar-em-cascais.jpg'),
+    MostFamousSpot(text: 'Margin of the Tagus', url: 'https://s7a5n8m2.stackpathcdn.com/wp-content/uploads/2015/03/riotejo.jpg')
   ];
 
   @override
@@ -26,38 +29,118 @@ class _MostFamousSpotsPage extends State<MostFamousSpotsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey.shade800,
+      backgroundColor: Colors.grey.shade900,
       appBar: AppBar(
-        backgroundColor: Colors.red,
-        title: Text('Choose the Route you want'),
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        toolbarHeight: 70,
+        title: Text("Most Famous Spots"),
         centerTitle: true,
-        elevation: 0,
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20),bottomRight: Radius.circular(20)),
+              gradient: LinearGradient(
+                  colors: [Colors.red,Colors.redAccent],
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter
+              )
+          ),
+        ),
       ),
-      body: ListView.builder(
-          itemCount: spots.length,
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.symmetric(vertical: 7.0, horizontal: 4.0),
-              child: Card(
-                color: Colors.amber,
-                child: ListTile(
-                    title: Text(spots[index].text),
-
-                    onTap: () {
-                      if (spots[index].text == 'Must See Spots in Central Lisbon')
+      body: Container(
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: spots.length,
+                itemBuilder: (BuildContext ctx, int index){
+                  return Container(
+                    margin: EdgeInsets.all(15),
+                    height: 150,
+                    child: InkWell(
+                      onTap: () {
                         Navigator.push(
                           context,
-                          MaterialPageRoute(
-                              builder: (context) => MustSeeCentralLisbonPage()),
+                          MaterialPageRoute(builder: (context) => MustSeeCentralLisbonPage()),
                         );
-                      else;
-                    },
-
-                ),
+                      },
+                      child: Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          Positioned.fill( //guarantees the image fills the stack area
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(15),
+                              child: Image.network(spots[index].url, fit: BoxFit.cover),
+                            )
+                          ),
+                          Positioned(
+                            bottom: 0,
+                            left: 0,
+                            right: 0,
+                            child: Container(
+                              height: 120,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.only(
+                                  bottomLeft: Radius.circular(15),
+                                  bottomRight: Radius.circular(15)
+                                ),
+                                gradient: LinearGradient(
+                                  begin: Alignment.bottomCenter,
+                                  end: Alignment.topCenter,
+                                  colors: [
+                                    Colors.black.withOpacity(0.4),
+                                    Colors.transparent
+                                  ]
+                                )
+                              ),
+                            )
+                          ),
+                          Positioned(
+                            bottom: 13.5,
+                            left: 35,
+                            right: 35,
+                            child: Container(
+                              height: 40,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                gradient: LinearGradient(
+                                  begin: Alignment.centerLeft,
+                                  end: Alignment.centerRight,
+                                  colors: [
+                                    Colors.black.withOpacity(0.3),
+                                    Colors.black.withOpacity(0.4)
+                                  ]
+                                )
+                              ),
+                            ),
+                          ),
+                          Positioned(
+                            bottom: 20,
+                            child: Align(
+                              alignment: FractionalOffset.bottomCenter,
+                              child: Row(
+                                children: [
+                                  Text(spots[index].text,
+                                    textAlign: TextAlign.end,
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 25
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      )
+                    )
+                  );
+                },
               ),
-            );
-          }
-      ),
+            )
+          ],
+        )
+      )
     );
   }
 }
--- lib/pages/route_page.dart
+++ lib/pages/route_page.dart
@@ -7,17 +7,18 @@ class RoutePage extends StatefulWidget {
 class Route {
   String icon;
   String text;
+  String url;
 
-  Route({this.icon, this.text});
+  Route({this.icon, this.text, this.url});
 }
 class _RoutePage extends State<RoutePage> {
 
   List<Route> routes = [
-    Route(icon: 'mostfamousspots.png', text: 'Most Famous Spots'),
-    Route(icon: 'quickroutes.png', text: 'Quick Routes'),
-    Route(icon: 'lesserknownspots.png', text: 'Lesser Known Spots'),
-    Route(icon: 'historicspots.png', text: 'Historic Spots'),
-    Route(icon: 'recentlyadded.png', text: 'Recently Added')
+    Route(icon: 'mostfamousspots.png', text: 'Most Famous Spots', url: 'https://offloadmedia.feverup.com/lisboasecreta.co/wp-content/uploads/2018/06/19182104/svetlana-gumerova-gOLCAOuc7iA-unsplash-scaled.jpg'),
+    Route(icon: 'quickroutes.png', text: 'Quick Routes', url: 'https://www.vendus.com/ao/foto/part-time-1200_og.jpg'),
+    Route(icon: 'lesserknownspots.png', text: 'Lesser Known Spots', url: 'https://media.tacdn.com/media/attractions-splice-spp-674x446/0b/39/80/c0.jpg'),
+    Route(icon: 'historicspots.png', text: 'Historic Spots', url: 'https://www.agendalx.pt/content/uploads/2019/12/castelosaojorgelisboa3473.jpg'),
+    Route(icon: 'recentlyadded.png', text: 'Recently Added', url: 'https://mediawiki.ivao.aero/images/9/9e/NEW.jpg')
   ];
 
   @override
@@ -28,37 +29,118 @@ class _RoutePage extends State<RoutePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey.shade800,
-      appBar: AppBar(
-        backgroundColor: Colors.red,
-        title: Text('Choose the Type of Route you want'),
-        centerTitle: true,
-        elevation: 0,
-      ),
-      body: ListView.builder(
-          itemCount: routes.length,
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.symmetric(vertical: 7.0, horizontal: 4.0),
-              child: Card(
-                color: Colors.amber,
-                child: ListTile(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => MostFamousSpotsPage()),
-                    );
-                  },
-                  title: Text(routes[index].text),
-                  leading: CircleAvatar(
-                    backgroundImage: AssetImage('assets/${routes[index].icon}'),
-                    backgroundColor: Colors.transparent,)
-
-                ),
-              ),
-            );
-          }
-      ),
+        backgroundColor: Colors.grey.shade900,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+          toolbarHeight: 70,
+          title: Text("Types of Routes"),
+          centerTitle: true,
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20),bottomRight: Radius.circular(20)),
+                gradient: LinearGradient(
+                    colors: [Colors.red,Colors.redAccent],
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topCenter
+                )
+            ),
+          ),
+        ),
+        body: Container(
+            child: Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: routes.length,
+                    itemBuilder: (BuildContext ctx, int index){
+                      return Container(
+                          margin: EdgeInsets.all(15),
+                          height: 150,
+                          child: InkWell(
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(builder: (context) => MostFamousSpotsPage()),
+                                );
+                              },
+                              child: Stack(
+                                alignment: Alignment.center,
+                                children: [
+                                  Positioned.fill( //guarantees the image fills the stack area
+                                      child: ClipRRect(
+                                        borderRadius: BorderRadius.circular(15),
+                                        child: Image.network(routes[index].url, fit: BoxFit.cover),
+                                      )
+                                  ),
+                                  Positioned(
+                                      bottom: 0,
+                                      left: 0,
+                                      right: 0,
+                                      child: Container(
+                                        height: 120,
+                                        decoration: BoxDecoration(
+                                            borderRadius: BorderRadius.only(
+                                                bottomLeft: Radius.circular(15),
+                                                bottomRight: Radius.circular(15)
+                                            ),
+                                            gradient: LinearGradient(
+                                                begin: Alignment.bottomCenter,
+                                                end: Alignment.topCenter,
+                                                colors: [
+                                                  Colors.black.withOpacity(0.4),
+                                                  Colors.transparent
+                                                ]
+                                            )
+                                        ),
+                                      )
+                                  ),
+                                  Positioned(
+                                    bottom: 13.5,
+                                    left: 35,
+                                    right: 35,
+                                    child: Container(
+                                      height: 40,
+                                      decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.circular(10),
+                                          gradient: LinearGradient(
+                                              begin: Alignment.centerLeft,
+                                              end: Alignment.centerRight,
+                                              colors: [
+                                                Colors.black.withOpacity(0.3),
+                                                Colors.black.withOpacity(0.4)
+                                              ]
+                                          )
+                                      ),
+                                    ),
+                                  ),
+                                  Positioned(
+                                    bottom: 20,
+                                    child: Align(
+                                      alignment: FractionalOffset.bottomCenter,
+                                      child: Row(
+                                        children: [
+                                          Text(' ' + routes[index].text,
+                                            textAlign: TextAlign.end,
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 25
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              )
+                          )
+                      );
+                    },
+                  ),
+                )
+              ],
+            )
+        )
     );
   }
 }
